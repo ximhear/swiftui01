9b2dcbfa7f3ee03afefc373dfbabd6fce12083e6
gzonelee <gzonelee@gmail.com>
2023-04-30 01:18:26 +0900

    .

---
+++ SwiftUI01/SwiftUI01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI01App.swift
+//  SwiftUI01
+//
+//  Created by gzonelee on 2023/04/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
