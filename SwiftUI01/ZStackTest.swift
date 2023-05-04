9e62cbb7130231f7784d9f1d576aa7f6acaf799a
chlee <z000000375@wemakeprice.com>
2023-05-04 18:02:54 +0900

    GetSizeModifier을 구현해 봄.

--- SwiftUI01/ZStackTest.swift
+++ SwiftUI01/ZStackTest.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 struct ZStackTest: View {
-    @State var size: CGSize = .zero
+    @State var size1: CGSize = .zero
+    @State var size2: CGSize = .zero
     @State var offset: CGSize = .zero
     @State var move: Bool = false
     var body: some View {
         GeometryReader { proxy in
             VStack {
-                Text(size.dimension)
+                Text(size1.dimension)
                 Button("animate") {
                     withAnimation(.interpolatingSpring(stiffness: 29.9, damping: 0.3)) {
                         move = !move
@@ -22,34 +23,39 @@ struct ZStackTest: View {
                 }
                 ZStack {
                     GeometryReader { proxy in
-                        Rectangle()
-                            .fill(move ? .blue.opacity(0.3) : .red.opacity(0.3))
-                            .frame(width: 100, height: 200)
-                            .background(
-                                GeometryReader { proxy in
-                                    Color.clear
-                                        .preference(key: SizePreferenceKey.self, value: proxy.size)
-                                }
-                            )
-                            .offset(move ? .init(width: 100, height: 100) : .zero)
+                        Group {
+                            Rectangle()
+                                .fill(move ? .blue.opacity(0.3) : .red.opacity(0.3))
+                                .frame(width: 100, height: 200)
+                                .background(
+                                    GeometryReader { proxy in
+                                        Color.clear
+                                            .preference(key: SizePreferenceKey.self, value: proxy.size)
+                                    }
+                                )
+                                .offset(move ? .init(width: 100, height: 100) : .zero)
+                        }
+                        .onPreferenceChange(SizePreferenceKey.self) { preferences in
+                            self.size1 = preferences
+                        }
                         Capsule()
                             .fill(.cyan.opacity(0.3))
                             .frame(width: 100, height: 200)
                             .offset(.init(width: 200, height: 100))
-                        Ellipse()
-                            .fill(.orange.opacity(0.3))
-                            .frame(width: 100, height: 200)
-                            .offset(.init(width: 100, height: 100))
-                            .onTapGesture {
-                                GZLogFunc()
-                            }
+                        Group {
+                            Ellipse()
+                                .fill(.orange.opacity(0.3))
+                                .frame(width: 100, height: 250)
+                                .offset(.init(width: 100, height: 100))
+                                .onTapGesture {
+                                    GZLogFunc()
+                                }
+                                .modifier(GetSizeModifier(size: $size2))
+                        }
                     }
                 }
                 .background(.purple.opacity(0.2))
-                .onPreferenceChange(SizePreferenceKey.self) { preferences in
-                    self.size = preferences
-                }
-                Text(size.dimension)
+                Text(size2.dimension)
             }
             .background(.yellow .opacity(0.1))
         }
@@ -74,5 +80,29 @@ extension CGSize {
 struct ZStackTest_Previews: PreviewProvider {
     static var previews: some View {
         ZStackTest()
+    }
+}
+
+struct GetSizeModifier: ViewModifier {
+    var size: Binding<CGSize>
+    init(size: Binding<CGSize>) {
+        self.size = size
+    }
+   
+    func body(content: Content) -> some View {
+        Group {
+            Group {
+                content
+            }
+            .background(
+                GeometryReader { proxy in
+                    Color.clear
+                        .preference(key: SizePreferenceKey.self, value: proxy.size)
+                }
+            )
+        }
+        .onPreferenceChange(SizePreferenceKey.self) { preferences in
+            self.size.wrappedValue = preferences
+        }
     }
 }
