2b1466e6e3a0ace6488d6ff13596a94dde1bfc76
chlee <z000000375@wemakeprice.com>
2023-05-24 10:29:21 +0900

    Update MagnificationGestureTest.swift

--- SwiftUI01/Gesture/Magnification/MagnificationGestureTest.swift
+++ SwiftUI01/Gesture/Magnification/MagnificationGestureTest.swift
@@ -13,6 +13,9 @@ struct MagnificationGestureTest: View {
     @GestureState private var amount1: CGFloat = 1
     @State private var amount2: CGFloat = 1
     
+    @State private var scale3: CGFloat = 1
+    @GestureState private var amount3: CGFloat = 1
+    
     var body: some View {
         VStack {
             Text("\(amount1 * scale1)")
@@ -26,8 +29,27 @@ struct MagnificationGestureTest: View {
                         GZLogFunc(value)
                         state = value
                     }.onEnded { (value) in
+                        // magniture는 절대값을 리턴해준다.
                         GZLogFunc(value.magnitude)
                         scale1 = scale1 * value.magnitude
+                    }
+                )
+            Divider()
+                .padding()
+            Text("limit \(amount3 * scale3)")
+                .zIndex(1)
+            Image("image01")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .scaleEffect(scale3 * amount3)
+                .gesture(
+                    MagnificationGesture().updating($amount3) { (value, state, _) in
+                        GZLogFunc(value)
+                        let scale = min(max(scale3 * value, 0.5), 3.0)
+                        state = scale / scale3
+                    }.onEnded { (value) in
+                        GZLogFunc(value)
+                        scale3 = min(max(scale3 * value, 0.5), 3.0)
                     }
                 )
             Divider()
