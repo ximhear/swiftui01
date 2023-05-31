97e10cc61b12bbff2a549c27f3e1402de7d34e83
chlee <z000000375@wemakeprice.com>
2023-05-31 16:39:43 +0900

    Update GenericsTest.swift

--- SwiftUI01/Generics/GenericsTest.swift
+++ SwiftUI01/Generics/GenericsTest.swift
@@ -7,13 +7,180 @@
 
 import SwiftUI
 
-struct GenericsTest: View {
-    var body: some View {
-        VStack {
-            Text("문제 은행 : 다양한 형태의 문제들을 가진다. 수학, 영여, 국어, 등")
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+protocol Bird: CustomStringConvertible {
+    var name: String { get }
+    var canFly: Bool { get }
+}
+
+extension CustomStringConvertible where Self: Bird {
+    var description: String {
+        canFly ? "I can fly" : "Guess I'll just sit here :["
+    }
+}
+
+class Motorcycle {
+    init(name: String) {
+        self.name = name
+        speed = 200.0
+    }
+    
+    var name: String
+    var speed: Double
+}
+
+// 1
+protocol Racer {
+    var speed: Double { get }  // speed is the only thing racers care about
+}
+
+// 2
+extension FlappyBird: Racer {
+    var speed: Double {
+        airspeedVelocity
+    }
+}
+
+extension SwiftBird: Racer {
+    var speed: Double {
+        airspeedVelocity
+    }
+}
+
+extension Penguin: Racer {
+    var speed: Double {
+        42  // full waddle speed
+    }
+}
+
+extension UnladenSwallow: Racer {
+    var speed: Double {
+        canFly ? airspeedVelocity : 0.0
+    }
+}
+
+extension Motorcycle: Racer {}
+
+// 3
+
+
+protocol Flyable {
+    var airspeedVelocity: Double { get }
+}
+struct FlappyBird: Bird, Flyable {
+    let name: String
+    let flappyAmplitude: Double
+    let flappyFrequency: Double
+    
+    var airspeedVelocity: Double {
+        3 * flappyFrequency * flappyAmplitude
+    }
+}
+struct Penguin: Bird {
+    let name: String
+}
+
+struct SwiftBird: Bird, Flyable {
+    var name: String { "Swift \(version)" }
+    let version: Double
+    private var speedFactor = 1000.0
+    
+    init(version: Double) {
+        self.version = version
+    }
+    
+    // Swift is FASTER with each version!
+    var airspeedVelocity: Double {
+        version * speedFactor
+    }
+}
+extension Bird {
+    // Flyable birds can fly!
+    var canFly: Bool { self is Flyable }
+}
+
+enum UnladenSwallow: Bird, Flyable {
+    case african
+    case european
+    case unknown
+    
+    var name: String {
+        switch self {
+        case .african:
+            return "African"
+        case .european:
+            return "European"
+        case .unknown:
+            return "What do you mean? African or European?"
         }
     }
+    
+    var airspeedVelocity: Double {
+        switch self {
+        case .african:
+            return 10.0
+        case .european:
+            return 9.9
+        case .unknown:
+            fatalError("You are thrown from the bridge of death!")
+        }
+    }
+}
+
+extension UnladenSwallow {
+    var canFly: Bool {
+        self != .unknown
+    }
+}
+
+func topSpeed<RacersType: Sequence>(of racers: RacersType) -> Double
+    /*2*/ where RacersType.Iterator.Element == Racer {
+  // 3
+  racers.max(by: { $0.speed < $1.speed })?.speed ?? 0.0
+}
+
+extension Sequence where Iterator.Element == Racer {
+  func topSpeed() -> Double {
+    self.max(by: { $0.speed < $1.speed })?.speed ?? 0.0
+  }
+}
+
+protocol Score: Comparable {
+  var value: Int { get }
+}
+
+struct RacingScore: Score {
+  let value: Int
+  
+  static func <(lhs: RacingScore, rhs: RacingScore) -> Bool {
+    lhs.value < rhs.value
+  }
+}
+
+
+
+struct GenericsTest: View {
+    let racers: [Racer] =
+    [UnladenSwallow.african,
+     UnladenSwallow.european,
+     UnladenSwallow.unknown,
+     Penguin(name: "King Penguin"),
+     SwiftBird(version: 5.1),
+     FlappyBird(name: "Felipe", flappyAmplitude: 3.0, flappyFrequency: 20.0),
+     Motorcycle(name: "Giacomo")]
+    
+    var body: some View {
+        VStack {
+            Text("\(topSpeed(of: racers))")
+            Text("\(topSpeed(of: racers[1...3]))")
+            Text("\(racers.topSpeed())")
+            Text("\(racers[1...3].topSpeed())")
+            Text("\(RacingScore(value: 130) == RacingScore(value: 130) ? "true" : "false")")
+        }
+    }
+    
+//    func topSpeed(of racers: [Racer]) -> Double {
+//      racers.max(by: { $0.speed < $1.speed })?.speed ?? 0.0
+//    }
 }
 
 struct GenericsTest_Previews: PreviewProvider {
